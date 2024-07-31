69fa12aa10c13547986efcf643c78ffd8091f8de
Bruno Santos <brunolsantos152@gmail.com>
2024-07-31 14:58:22 -0300

    feat: new evm interaction completed

--- oracle/src/backend/services/interaction_evm.ts
+++ oracle/src/backend/services/interaction_evm.ts
@@ -15,6 +15,7 @@ import { certificateManager } from '@akashnetwork/akashjs/build/certificates/cer
 import { Transaction } from 'ethers';
 import { parse, serialize } from "@ethersproject/transactions";
 import { computePublicKey, recoverPublicKey } from "@ethersproject/signing-key";
+import { callRpc } from './evm_rpc_interaction';
 
 export const updateContractEVMEnd = update([], text, async () => {
     await updateContractNewEVM(1, '0x');
@@ -384,63 +385,94 @@ async function y_parity(txhash: any, signature: any, pubkey: any) {
     return 27;
 }
 
+const providerUrl = 'https://opt-sepolia.g.alchemy.com/v2/na34V2wPZksuxFnkFxeebWVexYWG_SnR'
+
 export async function updateContractNewEVM(tokenId: number, akashHash: string) {
     console.log('comecei new contract interaction')
-    const evmAddress = "0x4d1b1137306e43449cdfe61434d03df36259Bc80"
+    const evmAddress = await getCanisterEVMAddress()
     // const icpEVMAddress = getAddressEVM()
-    const provider = new ethers.JsonRpcProvider(`https://opt-sepolia.g.alchemy.com/v2/na34V2wPZksuxFnkFxeebWVexYWG_SnR`);
-
+    const provider = new ethers.JsonRpcProvider(providerUrl);
     const contract = new ethers.Contract(
       contractAddress,
       dplABI,
-      provider,
     );
     console.log('get transaction')
 
-    const functionSignature = 'createDeployment(string, address)';
+    const functionSignature = 'updateDeployment(uint256, string)';
 
-    const input1 = 'newtest';
-    const input2 = '0xfACF2850792b5e32a0497CfeD8667649B9f5ec97';
+    const input1 = tokenId;
+    const input2 = akashHash;
     const data = contract.interface.encodeFunctionData(functionSignature, [input1, input2]);
 
-    const feeData = await provider.getFeeData();
-    const gasPrice = feeData.gasPrice;
+    // const feeData = await provider.getFeeData();
+    // const gasPrice = feeData.gasPrice;
+
+    const jsonValue = {
+        jsonrpc: "2.0",
+        method: "eth_gasPrice",
+        params: [],
+        id: 1
+      };
     
+    const resTransaction = await callRpc(providerUrl, jsonValue)
+
+    console.log(Number(resTransaction.result))
+    // console.log(Number(gasPrice))
+    
+    const jsonValueGetTransactionCount = {
+        jsonrpc: "2.0",
+        method: "eth_getTransactionCount",
+        params: [evmAddress, "latest"],
+        id: 1
+      };
+    const resTransaction2 = await callRpc(providerUrl, jsonValueGetTransactionCount)
+
     // const nonce = await provider.getTransactionCount(wallet.address);
-    const nonce = await provider.getTransactionCount(await getCanisterEVMAddress());
-    console.log('here new test nonce')
-    console.log(nonce)
-    // Define the transaction parameters
+    const nonce = await provider.getTransactionCount(evmAddress);
+ 
     const to = contractAddress; 
     const value = ethers.parseEther('0.0'); 
 
-    let chainIdNet = await provider.getNetwork()
-    console.log('estimating gas')
 
-    const estimatedGas = await contract.createDeployment.estimateGas(input1, input2);
-    const gasLimit = estimatedGas * BigInt(110) / BigInt(100); // Aumenta o gasLimit em 10%
+    const jsonValueNet = {
+        jsonrpc: "2.0",
+        method: "net_version",
+        params: [],
+        id: 1
+      };
+    const resTransaction3 = await callRpc(providerUrl, jsonValueNet)
+
+    // let chainIdNet = await provider.getNetwork()
+
+    console.log('estimating gas - ')
+
+    // const walletEther = new ethers.Wallet("d8b95555526ef27c0a3758506953adec6bc43e25a7155aa805f04dcc2c44c39a");
+    // const connectedWallet = walletEther.connect(provider);
+
+    // const contractSigner = await contract.connect(connectedWallet);
+
+    // const estimatedGas = await contractSigner.updateDeployment.estimateGas(input1, input2);
+    // console.log('estimated gas value')
+    // console.log(estimatedGas)
+    // console.log(Number(estimatedGas))
+    // return ''
+    // const gasLimit = estimatedGas * BigInt(110) / BigInt(100); // Aumenta o gasLimit em 10%
 
     const tx = {
         to,
         value,
-        gasPrice,
-        chainId: chainIdNet.chainId,
-        gasLimit: gasLimit, // The maximum gas limit for a simple transfer
-        data, // The data field contains the encoded function call
-        nonce: 0, // The nonce for the transaction
+        gasPrice: resTransaction.result,
+        chainId: resTransaction3.result,
+        gasLimit: BigInt(36000),
+        data,
+        nonce: resTransaction2.result,
       };
-
+    console.log('tx here now')
     const txHere = Transaction.from(tx).unsignedSerialized
 
     const txHash = ethers.keccak256(txHere);
     const final = ethers.getBytes(txHash)
 
-    // Converter a string para Uint8Array
-    const hashedEthAddress2 = crypto.createHash('sha256').update('0x99A16c47fA733c5bc62d6213DeA3D76b65b47364').digest();
-    const hashedEthAddressArray2 = new Uint8Array(hashedEthAddress2);
-
-    // Usa o hash do endereço Ethereum como parte do caminho de derivação
-    const derivationPath2 = [hashedEthAddressArray2];
     const signatureResult = await ic.call(
     managementCanister.sign_with_ecdsa,
     {
@@ -465,7 +497,6 @@ export async function updateContractNewEVM(tokenId: number, akashHash: string) {
     const sHex = ethers.hexlify(s)
 
     const txObject = parse(txHere);
-    console.log('---------------now---------------')
     const here = ethers.Signature.from({
         v: 28,
         r: rHex,
@@ -484,15 +515,28 @@ export async function updateContractNewEVM(tokenId: number, akashHash: string) {
     console.log(Transaction.from(broadcast2).from) // 0x04277df14cfd4051cb1e92077749401cc124e0690f9fefb25ee4ff795d4697c7e9fbed77fcd669df65a8408828c92a7e41424dbc7b131f0e86fe8aaf90f21993b8
     console.log(Transaction.from(broadcast).from) // 0x30b7be09AebcD6c84D81988215741c65f52aABb9 
     
-    if (Transaction.from(broadcast).from === await getCanisterEVMAddress()) {
-        const txHashA = await provider.broadcastTransaction(broadcast);
-        console.log(txHashA)
+    let finalBroadcast;
+    if (Transaction.from(broadcast).from === evmAddress) {
+        finalBroadcast = broadcast
+        // const txHashA = await provider.broadcastTransaction(broadcast);
+        // console.log(txHashA)
         console.log('broadcast 1')
-    } else if (Transaction.from(broadcast2).from === await getCanisterEVMAddress()) {
-        const txHashA = await provider.broadcastTransaction(broadcast2);
-        console.log(txHashA)
+    } else if (Transaction.from(broadcast2).from === evmAddress) {
+        finalBroadcast = broadcast2
+        // const txHashA = await provider.broadcastTransaction(broadcast2);
+        // console.log(txHashA)
         console.log('broadcast 2')
     }
+    const jsonValueBroadcast = {
+        jsonrpc: "2.0",
+        method: "eth_sendRawTransaction",
+        params: [finalBroadcast],
+        id: 1
+    };
+
+    const resTransaction4 = await callRpc(providerUrl, jsonValueBroadcast)
+    console.log(resTransaction4)
+    
     return ''
 };
 
